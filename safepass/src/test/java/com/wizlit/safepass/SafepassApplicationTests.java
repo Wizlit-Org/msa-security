31966d2756f3ee13ee7788f1b5afc9f535cb7a7a
Hminchae <mc.hwang@omotion.co.kr>
2025-03-27 15:11:48 +0900

    Add initial project

---
+++ safepass/src/test/java/com/wizlit/safepass/SafepassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizlit.safepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafepassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
