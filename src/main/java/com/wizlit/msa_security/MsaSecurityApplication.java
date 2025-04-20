147699785c0ee22d26d4fa4e9aa3761d5759594a
Hminchae <hminchae@gmail.com>
2025-04-20 21:09:40 +0900

    chore: initalize Spring Boot project

---
+++ src/main/java/com/wizlit/msa_security/MsaSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.wizlit.msa_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaSecurityApplication.class, args);
+	}
+
+}
