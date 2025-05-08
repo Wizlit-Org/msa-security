3f95cf39c956ff72f8155306722052092d691f58
Hminchae <hminchae@gmail.com>
2025-05-08 21:34:22 +0900

    Inital project commit

---
+++ src/test/java/com/wizlit/msasecurity/MsaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizlit.msasecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
